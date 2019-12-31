90da1f0b88c49fceb733fe3d7bb7707c6cfd0e37
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:23:56 -0600

    Rewriting as Laminas Project package

--- test/BigInteger/BigIntegerTest.php
+++ test/BigInteger/BigIntegerTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-math for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-math/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-math/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Math\BigInteger;
+namespace LaminasTest\Math\BigInteger;
 
-use Zend\Math\BigInteger\BigInteger as BigInt;
-use Zend\Math\BigInteger\Adapter;
-use Zend\Math\BigInteger\Adapter\AdapterInterface;
+use Laminas\Math\BigInteger\Adapter;
+use Laminas\Math\BigInteger\Adapter\AdapterInterface;
+use Laminas\Math\BigInteger\BigInteger as BigInt;
 
 /**
- * @group      Zend_Math
+ * @group      Laminas_Math
  */
 class BigIntegerTest extends \PHPUnit_Framework_TestCase
 {
@@ -48,7 +47,7 @@ class BigIntegerTest extends \PHPUnit_Framework_TestCase
 
     public function testFactoryUnknownAdapterRaisesServiceManagerException()
     {
-        $this->setExpectedException('Zend\ServiceManager\Exception\ExceptionInterface');
+        $this->setExpectedException('Laminas\ServiceManager\Exception\ExceptionInterface');
         BigInt::factory('unknown');
     }
 }
