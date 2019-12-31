c23a3978dad24d5a7ef295bd8ded54a279ce4d02
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:23:55 -0600

    Rewriting as Laminas Project package

--- src/BigInteger/Exception/ExceptionInterface.php
+++ src/BigInteger/Exception/ExceptionInterface.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-math for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-math/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-math/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Math\BigInteger\Exception;
+namespace Laminas\Math\BigInteger\Exception;
 
-use Zend\Math\Exception;
+use Laminas\Math\Exception;
 
 /**
  * Invalid argument exception
